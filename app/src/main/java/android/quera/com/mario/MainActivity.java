e127858f7d9ee2da8cb04e08baa3ab328143ea1a
Ali Yazdi <ma.yazdi75@aut.ac.ir>
2019-03-17 20:09:48 +0330

    add base project from android studio

---
+++ app/src/main/java/android/quera/com/mario/MainActivity.java
@@ -0,0 +1,15 @@
+package android.quera.com.mario;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
